79149238a95d07f80be56398e653ad13ef399180
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2022-12-07 00:11:06 -0800

    fix logo location

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,10 +1,6 @@
 <template>
-      <nuxt-img src="../public/apple-touch-icon.png" width="50" height="50"/>
+      <nuxt-img src="/apple-touch-icon.png" width="50" height="50"/>
       <span className="self-center ml-2 text-2xl font-extrabold whitespace-nowrap ">
           Timechain Academy
     </span>
 </template>
-
-<!-- 
-  <nuxt-img src="../public/apple-touch-icon.png" width="50" height="50"/>
--->
