ec9e090c65b233419d6a695dde7323645ccd932e
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2022-12-11 01:34:02 -0800

    move section css out to layout

--- components/Splash.vue
+++ components/Splash.vue
@@ -1,5 +1,6 @@
 <template>
-    <section class="bg-white md:bg-white/90 dark:bg-slate-900 dark:md:bg-slate-900 border-b dark:border-b-0">
+<section>
+<!-- <section class="bg-white md:bg-white/90 dark:bg-slate-900 dark:md:bg-slate-900 border-b dark:border-b-0"> -->
         <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
             <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
                 <div class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
