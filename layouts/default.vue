9c52d459e4d072d7c308d7d2627f86af04794e9a
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2022-12-11 19:15:14 -0800

    put back comment

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- https://color-mode.nuxtjs.org/#caveats -->
     <body class="bg-white md:bg-white/90 dark:bg-slate-900 dark:md:bg-slate-900 border-b dark:border-b-0"> 
       <slot />
     </body>
