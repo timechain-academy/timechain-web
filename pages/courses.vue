3370b69cc02212cf3d9d5924b7cb27e397491b14
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2022-12-13 01:43:38 -0800

    markdown table for courses

--- pages/courses.vue
+++ pages/courses.vue
@@ -6,38 +6,49 @@
                 Courses
             </h1>
             <p class="py-6 text-gray-900 dark:text-white">
-                Description about the course setup here. 
+               Current courses offered by our fabulous instructors.
             </p>
-            <div class="py-6">
-                <table class="table-auto bg-white dark:bg-slate-900 dark:text-white ">
+            <div class="p-6">
+                <ContentDoc path="/courses" class="table-auto prose dark:prose-invert justify-center" />
+                <!-- <table class="table-auto bg-white dark:bg-slate-900 dark:text-white ">
                     <thead>
                         <tr>
                             <th class="px-4 py-2">Title</th>
-                            <th class="px-4 py-2">Author</th>
-                            <th class="px-4 py-2">Views</th>
+                            <th class="px-4 py-2">Instructor</th>
+                            <th class="px-4 py-2">Start Date</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <td class="border px-4 py-2">Intro to CSS</td>
                             <td class="border px-4 py-2">Adam</td>
-                            <td class="border px-4 py-2">858</td>
+                            <td class="border px-4 py-2">10/8/2022</td>
                         </tr>
                         <tr>
                             <td class="border px-4 py-2 dark-mode:bg-slate-400">A Long and Winding Tour of the History of UI Frameworks and
                                 Tools and the Impact on Design</td>
                             <td class="border px-4 py-2">Adam</td>
-                            <td class="border px-4 py-2">112</td>
+                            <td class="border px-4 py-2">11/9/2022</td>
                         </tr>
                         <tr>
                             <td class="border px-4 py-2">Intro to JavaScript</td>
                             <td class="border px-4 py-2">Chris</td>
-                            <td class="border px-4 py-2">1,280</td>
+                            <td class="border px-4 py-2">12/2/2022</td>
                         </tr>
                     </tbody>
-                </table>
+                </table> -->
             </div>
+            <section>
+              <p class="dark:text-white p-6 flex items-center">
+                Interested in teaching? 
+                <a class="font-medium font-bold text-orange-600  hover:text-gray-400 dark:hover:text-gray px-4 py-3 flex items-center transition duration-150 ease-in-out"
+                href="https://github.com/timechainacademy/timechain-web/tree/master/content/courses.md">
+                + Add your Course
+                </a>       
+                </p>
+            </section>
         </div>
+        <Footer/>
         </NuxtLayout>
 </template>
 
