aad8288e245bf80659193745e9d513e70cd21d88
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2022-12-12 21:16:29 -0800

    initial card template

---
+++ components/Card.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="md:flex rounded-lg p-6">
+        <img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
+                src="/tca-icon.png"/>
+        <h2 class="text-lg dark:text-gray-900 p-6">
+            <a href="/" class="dark:text-white dark:hover:white hover:grey">Your Name</a>
+        </h2>
+    </div>
+</template>
