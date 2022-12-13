43e8772be10a97cb724c67b413bd804fe71ccfad
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2022-12-13 00:28:16 -0800

    justify cards to center

--- components/Creators.vue
+++ components/Creators.vue
@@ -1,11 +1,11 @@
 <template>
-<div class="grid grid-cols-2 grid-rows-1 gap-4  dark:text-white">
-    <div class="text-center md:flex" v-for="item in items">
+<div class="grid grid-cols-2 grid-rows-1 gap-2  dark:text-white">
+    <div class="justify-center md:flex" v-for="item in items">
         <div class="md:flex-shrink-0">
             <img class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto"
                 :src="item.imgpath"
-                alt="creator">
-             <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">{{item.title}}</div>
+                :alt="item.imgpath">
+             <div class="text-center uppercase tracking-wide text-sm text-indigo-600 font-bold">{{item.title}}</div>
             <a :href="item.link" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 dark:text-white hover:underline">
                 {{item.name}}
             </a>
