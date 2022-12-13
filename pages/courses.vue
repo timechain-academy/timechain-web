f235d43a406bb1b7b8778298e47d293f9d1990b4
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2022-12-13 02:08:08 -0800

    change color style

--- pages/courses.vue
+++ pages/courses.vue
@@ -14,7 +14,7 @@
             <section>
               <p class="dark:text-white p-6 flex items-center">
                 Interested in teaching? 
-                <a class="font-medium font-bold text-orange-600  hover:text-gray-400 dark:hover:text-gray px-4 py-3 flex items-center transition duration-150 ease-in-out"
+                <a class="font-medium font-bold text-indigo-600 dark:text-orange-300  hover:text-gray-400 dark:hover:text-gray px-4 py-3 flex items-center transition duration-150 ease-in-out"
                 href="https://github.com/bitkarrot/timechain-web/blob/master/content/courses.md">
                 + Add your Course
                 </a>       
