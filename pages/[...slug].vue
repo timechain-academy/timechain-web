77cd0f894763db543acc399caa722a0a68d3d67d
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2022-12-14 16:54:34 -0800

    reorder blog content

---
+++ pages/[...slug].vue
@@ -0,0 +1,18 @@
+<template>
+        <NuxtLayout :name="layout">
+            <Header />
+        
+            <div class="p-4 text-center pb-10 md:pb-16 max-w-4xl mx-auto">
+                <p class="dark:text-white">{{ $route.params }}</p>
+                <article class="">
+                    <ContentDoc path="/blog" class="p-2 prose dark:prose-invert text-left" />
+                </article>
+            </div>
+
+        </NuxtLayout>
+
+</template>
+
+<script setup> 
+  const layout = "default";
+</script>
---
+++ pages/ablog.vue
@@ -0,0 +1,26 @@
+<template>
+	<NuxtLayout :name="layout">
+    	<Header />
+		<main class="bg-white md:bg-white/90 dark:bg-slate-900 dark:md:bg-slate-900 border-b dark:border-b-0">
+			<div class="text-center pb-10 md:pb-16 max-w-4xl mx-auto">
+				<h1 class="py-5 mx-auto text-center text-5xl md:text-6xl text-gray-900 dark:text-white font-bold mb-6">
+					Blog
+				</h1>
+				<div class="p-5 mx-auto text-left text-gray-900 dark:text-white">
+						This code block below is generated in markdown
+						and added here by a `ContentDoc` tag. The section below is text in markdown.
+						CSS styles on this page are scoped to the blog.vue page.
+				</div>
+			</div>
+		</main>
+	</NuxtLayout>
+</template>
+
+<script setup> 
+  const layout = "default";
+</script>
+
+
+<style scoped>
+/* change .dark to be .dark-mode so that it can work with @nuxtjs/color-mode  */
+</style>
